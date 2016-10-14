a798d7be8eebc48db589024eb0e8471d44833021
Paul Foo <dev.paul.foo@gmail.com>
2016-10-15 00:33:08 +1000

    rate limit request finished started tweet search

--- modules/twitter.js
+++ modules/twitter.js
@@ -5,17 +5,88 @@
 var https = require('https');
 var btoa = require('btoa');
 var querystring = require('querystring');
+
 var hostName = 'api.twitter.com';
 var apiEndPoint = 'https://api.twitter.com';
-var apiVersion = '1.1';
-var requestRateLimit = '/' + apiVersion + '/application/rate_limit_status.json';
+var apiVersion = '/1.1';
 var tokenRequest = '/oauth2/token';
+var rateLimit = '/application/rate_limit_status.json?resources=search,application';
+var search = '/search/tweets.json';
+/**
+ * searchTweets
+ * @param req
+ * @param res
+ * @param token
+ */
+function searchTweets(req, res, token) {
+    var body = [];
 
+}
+
+function getRateLimit(req, res, token) {
+    var body = [];
+
+    var options = {
+        hostname: hostName,
+        path: apiVersion + rateLimit,
+        method: 'Get',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    };
+
+    console.log(options);
+
+    twitterReq = https.request(options, function(twitterRes) {
+
+        twitterRes.on('data', function(chunk) {
+           body.push(chunk);
+        });
+
+        twitterRes.on('end', function() {
+            var results = JSON.parse(body.join(''));
+
+            res.send(results);
+        });
+
+        twitterRes.on('error', function(e) {
+            console.log(e);
+            res.send(e);
+        })
+
+    });
+
+    twitterReq.end();
+
+}
+
+function searchTweetsPromise(req, res, token, hashTags) {
+    var body = [];
+    var path = search;
+    hashTags.forEach(function(tag) {
+        path = path + tag;
+    });
+
+    console.log(path);
+
+    path = encodeURIComponent(path);
+
+    console.log(path);
+
+    var options = {
+        hostname: hostName,
+        path: path,
+        method: 'Get'
+    }
+}
+
+/**
+ * requestAccessToken
+ * Obtains an access token object using application oauth.
+ * @param req
+ * @param res
+ */
 function requestAccessToken(req, res) {
-
-    // Post
-    // body of request grant_type=client_credentials
-    // application/x-www-form-urlencoded
 
     var body = [];
     var bearerTokenCredentials = process.env.CONSUMER_KEY + ':' + process.env.CONSUMER_SECRET;
@@ -23,10 +94,6 @@ function requestAccessToken(req, res) {
     var postData = querystring.stringify({
        'grant_type': 'client_credentials'
     });
-    console.log(bearerTokenCredentials);
-    console.log(bearerTokenCredentials64);
-    console.log(postData);
-    console.log(Buffer.byteLength(postData));
 
     var options = {
         hostname: hostName,
@@ -40,9 +107,9 @@ function requestAccessToken(req, res) {
     };
 
     twitterReq = https.request(options, function(twitterRes) {
-       twitterRes.on('data', function(chunk) {
-           body.push(chunk);
-       });
+        twitterRes.on('data', function(chunk) {
+            body.push(chunk);
+        });
 
         twitterRes.on('end', function() {
             var results = JSON.parse(body.join(''));
@@ -61,9 +128,56 @@ function requestAccessToken(req, res) {
 
     twitterReq.write(postData);
     twitterReq.end();
+}
+
+function requestAccessTokenPromise(req, res) {
+
+    var body = [];
+    var bearerTokenCredentials = process.env.CONSUMER_KEY + ':' + process.env.CONSUMER_SECRET;
+    var bearerTokenCredentials64 = btoa(bearerTokenCredentials);
+    var postData = querystring.stringify({
+        'grant_type': 'client_credentials'
+    });
+
+    var options = {
+        hostname: hostName,
+        path: tokenRequest,
+        method: 'Post',
+        auth: bearerTokenCredentials,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    return new Promise(function(resolve, reject) {
+        twitterReq = https.request(options, function(twitterRes) {
+            twitterRes.on('data', function(chunk) {
+                body.push(chunk);
+            });
+
+            twitterRes.on('end', function() {
+                var results = JSON.parse(body.join(''));
+                console.log(results);
+                resolve(results);
+            });
+
+            twitterRes.on('error', function(error) {
+                console.log('error');
+                reject(error);
+            })
+
+        });
+
+        twitterReq.write(postData);
+        twitterReq.end();
+    });
 
 }
 
+
 module.exports = {
-  'rateLimitRequest': requestAccessToken
+    "requestAccessToken": requestAccessToken,
+    "requestAccessTokenPromise": requestAccessTokenPromise,
+    "getRateLimit": getRateLimit
 };
--- routes/test.js
+++ routes/test.js
@@ -2,13 +2,48 @@ var express = require('express');
 var router = express.Router();
 var dotenv = require('dotenv').config();
 var twitter = require('../modules/twitter.js');
+var btoa = require('btoa');
+var sentiment = require('sentiment');
 
 /* For testing. */
 router.get('/', function(req, res, next) {
 
-    twitter.rateLimitRequest(req, res);
+    var tokenRequest = twitter.requestAccessTokenPromise(req, res);
 
+    tokenRequest.then(function(result) {
 
+        var token = result.access_token;
+        twitter.getRateLimit(req, res, token);
+
+    }).catch(function(error) {
+        res.send(error);
+    });
+
+});
+
+/**
+ * Route for testing the load on web server
+ *
+ */
+router.get('/sentiment', function(req, res, next) {
+
+    var text = 'the quick brown fox jumped over the lazy dog';
+
+    body = [];
+
+    for (var i = 0; i < 100; i++) {
+        var result = sentiment(text);
+        body.push(result);
+    }
+
+    res.send(body);
+
+});
+
+router.get('/rateLimit', function(req, res, next) {
+
+    var testToken = 'AAAAAAAAAAAAAAAAAAAAAMaDwgAAAAAAAxq3sFycKxpV1kdNqAn2e20o%2BTc%3DAIi849jeQgOVMpRDZxIBngWb6iyI4gT7pcOOu8LWyVbQbgEKn1';
+    twitter.getRateLimit(req, res, testToken);
 
 });
 
